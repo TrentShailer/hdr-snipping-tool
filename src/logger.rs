9918d2fb2e077eb7baeac7f9eafaaa515b1c7de2
Trent Shailer <trent.shailer@gmail.com>
2024-02-01 11:29:53 +1300

    Added file logging

--- src/logger.rs
+++ src/logger.rs
@@ -24,6 +24,7 @@ pub fn init_fern() -> Result<(), fern::InitError> {
         })
         .level(log::LevelFilter::Info)
         .chain(std::io::stdout())
+        .chain(fern::log_file("log.txt")?)
         .apply()?;
     Ok(())
 }
