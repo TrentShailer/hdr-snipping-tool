b11df5fbf648779c98863e9894891a960c33e334
Trent Shailer <trent.shailer@gmail.com>
2024-07-10 17:20:11 +1200

    display info holds whitepoint and luminance information

--- crates/hdr-capture/src/display_info.rs
+++ crates/hdr-capture/src/display_info.rs
@@ -3,10 +3,25 @@ use winit::dpi::{PhysicalPosition, PhysicalSize};
 pub struct DisplayInfo {
     pub size: PhysicalSize<u32>,
     pub position: PhysicalPosition<i32>,
+    pub maximum_luminance: f32,
+    pub sdr_whitepoint: f32,
+    pub sdr_whitepoint_nits: f32,
 }
 
 impl DisplayInfo {
-    pub fn new(size: PhysicalSize<u32>, position: PhysicalPosition<i32>) -> Self {
-        Self { size, position }
+    pub fn new(
+        size: PhysicalSize<u32>,
+        position: PhysicalPosition<i32>,
+        maximum_luminance: f32,
+        sdr_whitepoint: f32,
+        sdr_whitepoint_nits: f32,
+    ) -> Self {
+        Self {
+            size,
+            position,
+            maximum_luminance,
+            sdr_whitepoint,
+            sdr_whitepoint_nits,
+        }
     }
 }
