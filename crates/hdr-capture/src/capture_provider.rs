1ec0b49882367f5b049eddaf416cd3cf853bc9f0
Trent Shailer <trent.shailer@gmail.com>
2024-07-10 22:04:50 +1200

    removed some unused deps

--- crates/hdr-capture/src/capture_provider.rs
+++
@@ -1,8 +0,0 @@
-use crate::{CaptureInfo, DisplayInfo};
-
-pub trait CaptureProvider {
-    type Error: std::error::Error;
-
-    /// Get a raw capture and associated capture info.
-    fn get_capture(&mut self) -> Result<(Vec<u8>, DisplayInfo, CaptureInfo), Self::Error>;
-}
