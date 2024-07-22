7b7283f6bcdde4cf565a06616469e40e8673c290
Trent Shailer <trent.shailer@gmail.com>
2024-07-23 11:11:17 +1200

    fixed new logger

--- crates/hdr-snipping-tool/src/logger.rs
+++ crates/hdr-snipping-tool/src/logger.rs
@@ -3,35 +3,38 @@ use log::LevelFilter;
 use crate::project_directory;
 
 pub fn init_fern() -> Result<(), fern::InitError> {
-    fern::Dispatch::new()
-        .format(move |out, message, record| {
-            let message = message.to_string();
-            let time = chrono::Local::now().format("%F %r %:z").to_string();
-            let level = record.level();
-            let target = record.target();
-
-            out.finish(format_args!("[{time}] [{level}] [{target}]\n{message}\n",))
-        })
-        .chain(
-            fern::Dispatch::new()
-                .level(LevelFilter::Warn)
-                .chain(std::io::stdout())
-                .chain(fern::log_file(project_directory().join("error.log"))?),
-        )
-        .apply()?;
-
-    if std::env::var("hdr-snipping-tool-debug").is_ok() {
+    let logger = fern::Dispatch::new().chain(
         fern::Dispatch::new()
-            .format(move |out, message, _record| {
+            .format(move |out, message, record| {
                 let message = message.to_string();
-                out.finish(format_args!("{message}"))
+                let time = chrono::Local::now().format("%F %r %:z").to_string();
+                let level = record.level();
+                let target = record.target();
+
+                out.finish(format_args!("[{time}] [{level}] [{target}]\n{message}\n",))
             })
-            .filter(|metadata| metadata.level() == LevelFilter::Debug)
-            .level(LevelFilter::Debug)
+            .level(LevelFilter::Warn)
             .chain(std::io::stdout())
-            .chain(fern::log_file(project_directory().join("debug.log"))?)
-            .apply()?;
-    }
+            .chain(fern::log_file(project_directory().join("error.log"))?),
+    );
+
+    let logger = if std::env::var("hdr-snipping-tool-debug").is_ok() {
+        logger.chain(
+            fern::Dispatch::new()
+                .format(move |out, message, _record| {
+                    let message = message.to_string();
+                    out.finish(format_args!("{message}"))
+                })
+                .filter(|metadata| metadata.level() == LevelFilter::Debug)
+                .level(LevelFilter::Debug)
+                .chain(std::io::stdout())
+                .chain(fern::log_file(project_directory().join("debug.log"))?),
+        )
+    } else {
+        logger
+    };
+
+    logger.apply()?;
 
     Ok(())
 }
