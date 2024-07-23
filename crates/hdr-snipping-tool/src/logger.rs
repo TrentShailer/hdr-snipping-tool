5d4e8ab6a2741ae325f1abc146e8025e2cb48738
Trent Shailer <trent.shailer@gmail.com>
2024-07-23 19:21:22 +1200

    updated logger to better split default, debug, and verbose logging

--- crates/hdr-snipping-tool/src/logger.rs
+++ crates/hdr-snipping-tool/src/logger.rs
@@ -1,40 +1,91 @@
+use debug_helper::{is_debug, is_verbose};
 use log::LevelFilter;
 
 use crate::project_directory;
 
 pub fn init_fern() -> Result<(), fern::InitError> {
-    let logger = fern::Dispatch::new().chain(
-        fern::Dispatch::new()
-            .format(move |out, message, record| {
-                let message = message.to_string();
-                let time = chrono::Local::now().format("%F %r %:z").to_string();
-                let level = record.level();
-                let target = record.target();
+    if is_verbose() {
+        init_verbose_logger()?;
+    } else if is_debug() {
+        init_debug_logger()?;
+    } else {
+        init_default_logger()?;
+    };
 
-                out.finish(format_args!("[{time}] [{level}] [{target}]\n{message}\n",))
-            })
-            .level(LevelFilter::Warn)
-            .chain(std::io::stdout())
-            .chain(fern::log_file(project_directory().join("error.log"))?),
-    );
+    Ok(())
+}
 
-    let logger = if std::env::var("hdr-snipping-tool-debug").is_ok() {
-        logger.chain(
-            fern::Dispatch::new()
-                .format(move |out, message, _record| {
-                    let message = message.to_string();
-                    out.finish(format_args!("{message}"))
-                })
-                .filter(|metadata| metadata.level() == LevelFilter::Debug)
-                .level(LevelFilter::Debug)
+fn error_logger() -> fern::Dispatch {
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            let time = chrono::Local::now().format("%F %r %:z");
+            let level = record.level();
+            let target = record.target();
+
+            out.finish(format_args!("[{time}] [{level}] [{target}]\n{message}\n",))
+        })
+        .level(LevelFilter::Warn)
+}
+
+fn debug_logger() -> fern::Dispatch {
+    fern::Dispatch::new()
+        .format(move |out, message, _record| out.finish(format_args!("{message}")))
+        .level(LevelFilter::Debug)
+        .level_for("scrgb_tonemapper::tonemap", LevelFilter::Off)
+        .level_for("vulkan_renderer::renderer::render", LevelFilter::Off)
+}
+
+fn init_default_logger() -> Result<(), fern::InitError> {
+    error_logger()
+        .chain(std::io::stdout())
+        .chain(fern::log_file(project_directory().join("error.log"))?)
+        .apply()?;
+
+    Ok(())
+}
+
+fn init_debug_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .chain(error_logger().chain(fern::log_file(project_directory().join("error.log"))?))
+        .chain(
+            debug_logger()
                 .chain(std::io::stdout())
                 .chain(fern::log_file(project_directory().join("debug.log"))?),
         )
-    } else {
-        logger
-    };
+        .apply()?;
 
-    logger.apply()?;
+    Ok(())
+}
+
+fn init_verbose_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .chain(error_logger().chain(fern::log_file(project_directory().join("error.log"))?))
+        .chain(debug_logger().chain(fern::log_file(project_directory().join("debug.log"))?))
+        .chain(
+            fern::Dispatch::new()
+                .format(move |out, message, _record| out.finish(format_args!("{message}")))
+                .level(LevelFilter::Info)
+                .level_for("scrgb_tonemapper::tonemap", LevelFilter::Debug)
+                .chain(fern::log_file(
+                    project_directory().join("_tonemap.debug.log"),
+                )?),
+        )
+        .chain(
+            fern::Dispatch::new()
+                .format(move |out, message, _record| out.finish(format_args!("{message}")))
+                .level(LevelFilter::Info)
+                .level_for("vulkan_renderer::renderer::render", LevelFilter::Debug)
+                .chain(fern::log_file(
+                    project_directory().join("_render.debug.log"),
+                )?),
+        )
+        .chain(
+            fern::Dispatch::new()
+                .format(move |out, message, _record| out.finish(format_args!("{message}")))
+                .level(LevelFilter::Debug)
+                .chain(std::io::stdout()),
+        )
+        .apply()?;
 
     Ok(())
 }
