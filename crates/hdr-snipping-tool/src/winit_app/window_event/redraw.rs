2e1e820d712d568ce22524cd653925af14b29822
Trent Shailer <trent.shailer@gmail.com>
2024-09-08 19:46:04 +1200

    allow redraw without capture

--- crates/hdr-snipping-tool/src/winit_app/window_event/redraw.rs
+++ crates/hdr-snipping-tool/src/winit_app/window_event/redraw.rs
@@ -1,3 +1,5 @@
+use hdr_capture::Rect;
+
 use crate::winit_app::WinitApp;
 
 use super::Error;
@@ -8,19 +10,21 @@ impl WinitApp {
             return Ok(());
         };
 
-        let Some(capture) = self.capture.as_mut() else {
-            return Ok(());
-        };
-
         if !app.window.is_visible().unwrap_or(true) {
             return Ok(());
         }
 
-        app.renderer.render(
-            &app.window,
-            self.mouse_position.into(),
-            capture.selection.rect,
-        )?;
+        let selection_rect = if let Some(capture) = self.capture.as_ref() {
+            capture.selection.rect
+        } else {
+            Rect {
+                start: [0, 0],
+                end: [4096, 4096],
+            }
+        };
+
+        app.renderer
+            .render(&app.window, self.mouse_position.into(), selection_rect)?;
 
         Ok(())
     }
