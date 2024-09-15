5f2a7345b8add3c317997cc4b151fa74f62a7092
Trent Shailer <trent.shailer@gmail.com>
2024-09-15 14:35:09 +1200

    add send wrappers for various windows-rs types

--- crates/windows-capture-provider/src/lib.rs
+++ crates/windows-capture-provider/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod capture_item_cache;
 pub mod directx_devices;
 pub mod display;
+pub mod send_types;
 pub mod windows_capture;
 
 pub use capture_item_cache::{hovered, refresh, CaptureItemCache};
 pub use directx_devices::DirectXDevices;
 pub use display::Display;
+pub use send_types::{SendHANDLE, SendHMONITOR, SendIDirect3DDevice};
 pub use windows_capture::WindowsCapture;
---
+++ crates/windows-capture-provider/src/send_types/handle.rs
@@ -0,0 +1,21 @@
+use std::ops::{Deref, DerefMut};
+
+use windows::Win32::Foundation::HANDLE;
+
+pub struct SendHANDLE(pub HANDLE);
+
+unsafe impl Send for SendHANDLE {}
+
+impl Deref for SendHANDLE {
+    type Target = HANDLE;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for SendHANDLE {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
