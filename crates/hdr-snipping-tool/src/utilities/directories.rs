f8a311ed74a06ce2ce5f18b8fedd64339fc2315f
Trent Shailer <trent.shailer@gmail.com>
2025-05-31 15:22:42 +1200

    Improve resiliance against folder deletion

--- crates/hdr-snipping-tool/src/utilities/directories.rs
+++ crates/hdr-snipping-tool/src/utilities/directories.rs
@@ -1,22 +1,29 @@
 use std::fs::create_dir_all;
 
+use super::failure::{Failure, Ignore};
+
 /// Path to the screenshot directory.
 pub fn screenshot_dir() -> std::path::PathBuf {
-    dirs::picture_dir()
-        .expect("The dirs library does not support this platform's picture dir.")
-        .join("Screenshots")
+    let dir = dirs::picture_dir()
+        .report_and_panic("The picture directory could not be retreived")
+        .join("Screenshots");
+
+    create_dir_all(&dir)
+        .report("Could not create the screenshot directory")
+        .ignore();
+
+    dir
 }
 
 /// Path to the config directory.
 pub fn config_dir() -> std::path::PathBuf {
-    dirs::config_dir()
-        .expect("The dirs library does not support this patform's config dir.")
-        .join("HDR Snipping Tool")
-}
+    let dir = dirs::config_dir()
+        .report_and_panic("The config directory could not be retreived")
+        .join("HDR Snipping Tool");
 
-pub fn create_dirs() -> std::io::Result<()> {
-    create_dir_all(config_dir())?;
-    create_dir_all(screenshot_dir())?;
+    create_dir_all(&dir)
+        .report("Could not create the config directory")
+        .ignore();
 
-    Ok(())
+    dir
 }
